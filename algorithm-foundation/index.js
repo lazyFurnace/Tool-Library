648e29aacfa034d75810ff96855ff752e0f4c270
李齐 <787139384@qq.com>
2018-06-12 20:15:50 +0800

    Update index.js

--- algorithm-foundation/index.js
+++ algorithm-foundation/index.js
@@ -1,1 +1,25 @@
 //栈
+function Stack() {
+    let item = [];
+    this.push = function(...arg) {
+        item.push(...arg);
+    }
+    this.pop = function() {
+        return item.pop();
+    }
+    this.peek = function() {
+        return item[item.length - 1];
+    }
+    this.isEmpty = function() {
+        return item.length === 0;
+    }
+    this.clear = function() {
+        item.length = 0;
+    }
+    this.size = function() {
+        return item.length;
+    }
+    this.print = function() {
+        return item.toString();
+    }
+}
