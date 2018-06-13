b5ab48721ad0a50ffc753602ca95c21f7a880242
qi.li <787139384@qq.com>
2018-06-13 23:58:18 +0800

    队列

--- algorithm-foundation/index.js
+++ algorithm-foundation/index.js
@@ -23,3 +23,26 @@ function Stack() {
         return item.toString();
     }
 }
+
+//队列
+function Queue() {
+    let item = [];
+    this.enqueue = function(...arg) {
+        item.push(...arg);
+    }
+    this.dequeue = function() {
+        return item.unshift()
+    }
+    this.front = function() {
+        return item[0];
+    }
+    this.isEmpty = function() {
+        return item.length === 0;
+    }
+    this.size = function() {
+        return item.length;
+    }
+    this.print = function() {
+        return item.toString();
+    }
+}
