5e5a6b8b9ebf6b72f53940a13bb73e7fbd563b4c
李齐 <787139384@qq.com>
2018-08-15 20:36:54 +0800

    Update promise.js

--- promise.js
+++ promise.js
@@ -30,3 +30,50 @@ Promise.prototype.then = function(onFulfilled, onRejected) {
         throw Error '错误';
     }
 }
+
+
+
+function Promise(executor) {
+    const _ = this;
+    _.state = 'pending';
+    _.value = null;
+    _.callbacksResolve = [];
+    _.callbacksReject = [];
+    
+    function resolve(value) {
+        _.state = 'resolve';
+        _.value = value;
+        _.callbacksResolve.forEach(fn => fn(_.value))
+    }
+
+    function reject(value) {
+        _.state = 'reject';
+        _.value = value;
+        _.callbacksReject.forEach(fn => fn(_.value))
+    }
+    
+    executor(resolve, reject);
+}
+
+Promise.prototype.then = function(onFulfilled, onRejected) {
+    const _ = this;
+
+    if(_.state === 'pending') {
+
+        _.callbacksResolve.push(onFulfilled);
+        _.callbacksReject.push(onRejected);
+
+    } else if (_.state === 'resolve') {
+
+        onFulfilled(_.value);
+
+    } else if (_.state === 'reject') {
+
+        onRejected(_.value);
+
+    } else {
+
+        throw Error ('Promise 状态未知');
+
+    }
+}
